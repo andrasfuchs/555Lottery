86095386fb2cb2e6efa21d8134b583e1584908c4
Andras Fuchs <andras.fuchs@gmail.com>
2021-05-25 13:06:44 +0200

    Update a few outdated packages

--- Source/555Lottery.Web/Scripts/_references.js
+++ Source/555Lottery.Web/Scripts/_references.js
@@ -1,3 +1,3 @@
-﻿/// <reference path="jquery-ui-1.10.3.js" />
-/// <reference path="jquery-2.0.3.js" />
-/// <reference path="modernizr-2.6.2.js" />
+﻿/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="jquery-ui-1.12.1.js" />
+/// <reference path="jquery-3.6.0.js" />
